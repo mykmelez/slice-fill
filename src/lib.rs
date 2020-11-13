cc5c23922a9aee084f4abed6e78dcee2241e2187
Myk Melez <myk@junelife.com>
2020-11-12 16:23:38 -0800

    #![no_std]

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@
 //! assert_eq!(buf, vec![1; 10]);
 //! ```
 
+#![no_std]
 #![forbid(unsafe_code, future_incompatible, rust_2018_idioms)]
 #![deny(missing_debug_implementations, nonstandard_style)]
 #![warn(missing_docs, missing_doc_code_examples, unreachable_pub)]
